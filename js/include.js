081037e9bae1c1880aaa513c7a6994813eb12b7f
ph-taro <p.h.atab.tar0@gmail.com>
2025-08-26 16:08:47 +0900

    Create include.js

---
+++ js/include.js
@@ -0,0 +1,13 @@
+async function loadInclude(id, file) {
+  const el = document.getElementById(id);
+  if (el) {
+    const res = await fetch(file);
+    const text = await res.text();
+    el.innerHTML = text;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadInclude("header", "header.html");
+  loadInclude("footer", "footer.html");
+});
