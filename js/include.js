618840594be9b21adc36de672ba40907203af346
ph-taro <p.h.atab.tar0@gmail.com>
2025-08-26 16:20:42 +0900

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,7 +1,7 @@
 async function loadInclude(id, file) {
   const el = document.getElementById(id);
   if (el) {
-    const res = await fetch(file);
+    const res = await fetch(`inc/${file}`);
     const text = await res.text();
     el.innerHTML = text;
   }
